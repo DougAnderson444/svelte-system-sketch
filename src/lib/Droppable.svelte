d2860c657020075925ea89650ce2eebe41c15a64
Doug A <douganderson444@gmail.com>
2022-02-26 11:48:48 -0400

    fix drag and drop alorithm

--- src/lib/Droppable.svelte
+++ src/lib/Droppable.svelte
@@ -1,6 +1,7 @@
 <script>
 	import { groups } from '$lib/features/directives/draggable';
 
+	import _ from 'lodash-es';
 	import _get from 'lodash-es/get';
 	import _set from 'lodash-es/set';
 	import _remove from 'lodash-es/remove';
@@ -26,21 +27,29 @@
 		var old_g = ev.dataTransfer.getData('group');
 
 		// Move first, then drop element from array
-		const item = (old_g ? _get($groups, old_g) : $groups)[i];
+		// const item = (old_g ? _get($groups, old_g) : $groups)[i];
+		// const item = (old_g ? _get($groups, old_g) : $groups)?.splice(i, 1)[0]; // remove from old group
 
-		let newSpot = _get($groups, new_g);
+		let newGrp = _get($groups, new_g);
 
 		// if the child group doesnt exist, create it
-		if (!new_g) $groups.push(item);
+		if (!new_g)
+			$groups = [...$groups, (old_g ? _get($groups, old_g) : $groups)?.splice(i, 1)[0]].sort(
+				(f, s) => (f.children.length < s.children.length ? -1 : 1)
+			);
 		// add to root of group object
-		else if (!newSpot) _set($groups, new_g, [item]);
+		else if (!newGrp)
+			_set($groups, new_g, [(old_g ? _get($groups, old_g) : $groups)?.splice(i, 1)[0]]).sort(
+				(f, s) => (f.children.length < s.children.length ? -1 : 1)
+			);
 		// create the new group as it didn't exist
-		else newSpot.push(item); // exists, add item to new group
+		else _get($groups, new_g).push((old_g ? _get($groups, old_g) : $groups)?.splice(i, 1)[0]);
 
-		(old_g ? _get($groups, old_g) : $groups)?.splice(i, 1)[0]; // remove from old group
-
+		// sort by if the groups have children or not
 		$groups = $groups;
 	}
+
+	function sortGroup(group) {}
 </script>
 
 {#if name}
