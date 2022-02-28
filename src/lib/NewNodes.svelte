17ae4ecfebd45eabf39695c4f723841c4e0a0a5d
Doug A <douganderson444@gmail.com>
2022-02-28 13:13:01 -0400

    styling tweeks

--- src/lib/NewNodes.svelte
+++ src/lib/NewNodes.svelte
@@ -1,21 +1,23 @@
 <script>
-	import { dndzone, SHADOW_PLACEHOLDER_ITEM_ID } from 'svelte-dnd-action';
+	import { dndzone, SHADOW_PLACEHOLDER_ITEM_ID, TRIGGERS } from 'svelte-dnd-action';
 	import { flip } from 'svelte/animate';
 
-	import StyledRect from './atomic/StyledRect.svelte';
+	// import StyledRect from './atomic/StyledRect.svelte';
 
 	export let nodes;
 	export let node;
+	export let wrapper;
 
 	function handleDnd(e) {
 		nodes = e.detail.children;
 	}
 
-	const flipDurationMs = 300;
+	const flipDurationMs = 400;
 	const dropTargetStyle = { outline: '#51ff002c solid 2px' };
 
 	function handleDndConsider(e) {
 		node.children = e.detail.items;
+		console.log({ info: e.detail.info });
 	}
 	function handleDndFinalize(e) {
 		node.children = e.detail.items;
@@ -24,44 +26,50 @@
 	}
 
 	function transformDraggedElement(draggedEl, data, index) {
-		console.log({ draggedEl }, draggedEl.querySelector('.dragWrapper'));
-		draggedEl.querySelector('.item').style.boxShadow = '2px 2px 8px rgba(0, 0, 0, 0.1)'; // '5px 10px red'; // .transform = 'rotate(10deg)'; //.boxShadow = '5px 10px red';
-		draggedEl.querySelector('.item').style.outline = 'green solid 1px';
+		draggedEl.querySelector('.dragWrapper').style.boxShadow = '2px 2px 8px rgba(0, 0, 0, 0.8)'; // '5px 10px red'; // .transform = 'rotate(10deg)'; //.boxShadow = '5px 10px red';
+		draggedEl.querySelector('.dragWrapper').style.outline = 'green solid 1px';
 	}
 </script>
 
-{#if node}
-	<b style="color:{node?.color}">{node.name}</b>
-	{#if node.hasOwnProperty('children')}
-		<section
-			use:dndzone={{
-				items: node.children,
-				dropTargetStyle,
-				flipDurationMs,
-				transformDraggedElement,
-				centreDraggedOnCursor: false
-			}}
-			on:consider={handleDndConsider}
-			on:finalize={handleDndFinalize}
-		>
-			<!-- WE FILTER THE SHADOW PLACEHOLDER THAT WAS ADDED IN VERSION 0.7.4, filtering this way rather than checking whether 'nodes' have the id became possible in version 0.9.1 -->
-			{#each node.children.filter((item) => item.id !== SHADOW_PLACEHOLDER_ITEM_ID) as item (item.id)}
-				<div animate:flip={{ duration: flipDurationMs }} class="_item">
-					<div class="dragWrapper item">
-						<svelte:self bind:nodes node={item} />
-					</div>
-				</div>
-			{/each}
-		</section>
-	{:else}
-		Item
-	{/if}
+{#if node && wrapper}
+	<svelte:component this={wrapper}>
+		<div slot="header">
+			<b style="color:{node?.color}">{node.name}</b>
+		</div>
+		<div slot="children">
+			{#if node.hasOwnProperty('children')}
+				<section
+					class="dragndropSection"
+					use:dndzone={{
+						items: node.children,
+						dropTargetStyle,
+						flipDurationMs,
+						transformDraggedElement,
+						centreDraggedOnCursor: false
+					}}
+					on:consider={handleDndConsider}
+					on:finalize={handleDndFinalize}
+				>
+					<!-- WE FILTER THE SHADOW PLACEHOLDER THAT WAS ADDED IN VERSION 0.7.4, filtering this way rather than checking whether 'nodes' have the id became possible in version 0.9.1 -->
+					{#each node.children.filter((item) => item.id !== SHADOW_PLACEHOLDER_ITEM_ID) as item (item.id)}
+						<div animate:flip={{ duration: flipDurationMs }} class="_item">
+							<div class="dragWrapper item">
+								<svelte:self bind:nodes node={item} {wrapper} />
+							</div>
+						</div>
+					{/each}
+				</section>
+			{:else}
+				Item
+			{/if}
+		</div>
+	</svelte:component>
 {/if}
 
 <style>
-	section {
-		width: auto;
-		border: 2px dashed grey;
+	section.dragndropSection {
+		width: 100%;
+		border: 1px dashed rgba(128, 128, 128, 0.486);
 		border-radius: 5px;
 		padding: 0.4em;
 		/* this will allow the dragged element to scroll the list */
@@ -72,27 +80,10 @@
 	}
 	div {
 		width: 90%;
-		padding: 0.3em;
-		border: 0px solid blue;
-		margin: 0.15em 0;
+		padding: 0.05em;
+		margin: 0.05em 0;
 	}
 	.item {
-		background-color: rgba(00, 100, 100, 0.1);
-	}
-	.group-node {
-		border: 1px dashed grey;
-		padding: 9px;
-		margin: 9px;
-		font-size: 0.9em;
-	}
-	.leaf,
-	.draggableGroup {
-		width: fit-content;
-	}
-	.dropZoneDiv {
-		border: 1px dashed grey;
-		padding: 9px;
-		margin: 9px;
-		font-size: 0.9em;
+		background-color: rgba(00, 100, 100, 0.025);
 	}
 </style>
