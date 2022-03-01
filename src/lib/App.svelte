9e68c2008da5b24c79c3caf7d24f8eef7cf1416f
Doug A <douganderson444@gmail.com>
2022-03-01 11:36:33 -0400

    use nanoid

--- src/lib/App.svelte
+++ src/lib/App.svelte
@@ -1,60 +1,85 @@
 <script>
 	import { onMount } from 'svelte';
 	import Nodes from './Nodes.svelte';
-	import { groups } from '$lib/features/directives/draggable';
+	import NewNodes from './NewNodes.svelte';
+	import { nanoid } from 'nanoid';
+	import Wrapper from './atomic/Basic.svelte';
+	import GridControls from './GridControls.svelte';
 
 	let rectColor = '#' + (((1 << 24) * Math.random()) | 0).toString(16);
 
 	let canvas;
-
+	let mounted;
 	onMount(async () => {
 		import('svelte-drag-drop-touch');
+		mounted = true;
 	});
 
-	$groups = [
-		{
-			name: 'First Fruit basket',
-			color: 'brown',
-			children: [
-				{ name: 'Orange', color: 'orange', children: [] },
-				{ name: 'Pineapple', color: 'yellow', children: [] }
-			]
-		},
-		{
-			name: 'Second Fruit basket',
-			color: 'brown',
-			children: [
-				{ name: 'Banana', color: 'blue', children: [] },
-				{
-					name: 'Apple',
-					color: 'red',
-					children: [
-						{ name: 'Gala', color: 'red', children: [] },
-						{ name: 'Granny', color: 'lightgreen', children: [] }
-					]
-				}
-			]
-		},
-		{
-			name: 'Third Fruit basket',
-			color: 'brown',
-			children: [
-				{ name: 'Banana', color: 'blue', children: [] },
-				{
-					name: 'EXOTIC Apples',
-					color: 'red',
-					children: [
-						{ name: 'Gala', color: 'red', children: [] },
-						{ name: 'Granny', color: 'lightgreen', children: [] }
-					]
-				}
-			]
-		}
-	];
+	let groups = {
+		id: nanoid(),
+		name: 'Root',
+		color: 'red',
+		children: [
+			{
+				id: nanoid(),
+				name: 'First Fruit basket',
+				color: 'brown',
+				children: [
+					{ id: nanoid(), name: 'Orange', color: 'orange' },
+					{ id: nanoid(), name: 'Pineapple', color: 'yellow', children: [] }
+				]
+			},
+			{
+				id: nanoid(),
+				name: 'Second Fruit basket',
+				color: 'brown',
+				children: [
+					{ id: nanoid(), name: 'Banana', color: 'blue', children: [] },
+					{
+						id: nanoid(),
+						name: 'Apple',
+						color: 'red',
+						children: [
+							{ id: nanoid(), name: 'Gala', color: 'red', children: [] },
+							{ id: nanoid(), name: 'Granny', color: 'lightgreen', children: [] }
+						]
+					}
+				]
+			},
+			{
+				id: nanoid(),
+				name: 'Third Fruit basket',
+				color: 'brown',
+				children: [
+					{ id: nanoid(), name: 'Banana', color: 'blue', children: [] },
+					{
+						id: nanoid(),
+						name: 'EXOTIC Apples',
+						color: 'red',
+						children: [
+							{ id: nanoid(), name: 'Gala', color: 'red', children: [] },
+							{ id: nanoid(), name: 'Granny', color: 'lightgreen', children: [] }
+						]
+					}
+				]
+			}
+		]
+	};
+	let layoutNodes = {
+		id: nanoid(),
+		name: 'Root',
+		color: 'red',
+		children: [
+			{ id: nanoid(), name: 'Left', color: 'grey' },
+			{ id: nanoid(), name: 'TopRight', color: 'grey' },
+			{ id: nanoid(), name: 'BottomRight', color: 'grey' }
+		]
+	};
 </script>
 
-{#if $groups}
-	<Nodes nodes={$groups} name={'My Awesome Flex Drop Group'} group={''} item={''} />
+{#if mounted && groups}
+	<NewNodes bind:nodes={groups} node={groups} wrapper={Wrapper} />
+	<!-- <Nodes nodes={$groups} name={'My Awesome Flex Drop Group'} group={''} item={''} /> -->
 {/if}
 
 <style>
