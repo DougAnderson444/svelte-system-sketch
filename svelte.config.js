bc2456b96d3a87c957fdb1bfb2027fb21287a087
Doug A <douganderson444@gmail.com>
2022-11-25 20:51:03 -0400

    use path

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
 // import adapter from '@sveltejs/adapter-auto';
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
+import path from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
